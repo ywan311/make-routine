29021975e9f2810a514e7e1c3db2f5940b5079bf
ywan311 <ywan311@naver.com>
2024-02-17 18:36:48 +0900

    init : ㅅㅣ작해 봅시다!

---
+++ src/main/java/com/yoowan/makeroutine/MakeRoutineApplication.java
@@ -0,0 +1,13 @@
+package com.yoowan.makeroutine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeRoutineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeRoutineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoowan/makeroutine/MakeRoutineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoowan.makeroutine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeRoutineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
